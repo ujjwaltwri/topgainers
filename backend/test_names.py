fb2a2f11ebcae2d1c37814db5ee338671aef2ee6
ujjwal <tiwariujjwal33@gmail.com>
2026-06-24 02:22:23 +0530

    Convert mobile sidebar into a sliding overlay drawer

--- backend/test_names.py
+++
@@ -1,9 +0,0 @@
-import financedatabase as fd
-import yfinance as yf
-
-equities = fd.Equities()
-df_eq = equities.select(country="South Korea")
-print(df_eq.head())
-
-t = yf.Ticker("352770.KS")
-print(t.info.get('shortName'), t.info.get('longName'))
--- backend/test_pykrx.py
+++
@@ -1,5 +0,0 @@
-from pykrx import stock
-
-print(stock.get_market_ticker_name("352770"))
-print(stock.get_market_ticker_name("080220"))
-print(stock.get_market_ticker_name("079650"))
